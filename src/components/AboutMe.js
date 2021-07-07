c85932c9eb63227ec4d684082fb225f0d18b1369
Asha Sanford <sanford.asha97@yahoo.com>
2021-07-06 22:59:15 -0400

    added about me component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+      <div>
+          <h1>About Me</h1>
+          <p>Asha Sanford is a Full Stack Web Developer and a Animation major. </p>
+          <img alt="placeholder-profile-image" src="https://www.psifoundation.org/wp-content/uploads/2018/03/placeholder-300x400.png"></img>
+      </div>
+    );
+  }
+
+export default AboutMe;
