f1b31e11351d52a619fd17c5d40cadd0d52b8f91
Asha Sanford <sanford.asha97@yahoo.com>
2021-07-14 21:24:04 -0400

    added bootstrap in index.js.  added white border in index.css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function AboutMe() {
     return (
-      <div class="container">
+      <div id="aboutme" class="container">
           <h1>About Me</h1>
-          <p>Asha Sanford is an Atlanta based Full Stack Web Developer and Animation major.I have a Bachelor's degree in Animation and a minor in Motion Graphics. I attended and graduated from Savannah College of Art and Design back in 2019. I have also worked as an Assistant Art teacher for approximately 3 years now. Web Design caught my attention once I realized how interexhancable technical skills can be across all different industries and jobs. I started the Georgia Tech Coding bootcamp in February 2021, and expect to complete by August 2021.
+          <p id= "abouttext">Asha Sanford is an Atlanta based Full Stack Web Developer and Animation major.I have a Bachelor's degree in Animation and a minor in Motion Graphics. I attended and graduated from Savannah College of Art and Design back in 2019. I have also worked as an Assistant Art teacher for approximately 3 years now. Web Design caught my attention once I realized how interexhancable technical skills can be across all different industries and jobs. I started the Georgia Tech Coding bootcamp in February 2021, and expect to complete by August 2021.
 
 </p>
           <img alt="placeholder-profile-image" src="https://www.psifoundation.org/wp-content/uploads/2018/03/placeholder-300x400.png"></img>
